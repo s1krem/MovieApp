bb97c477464863daf321a99cf0f803336f151edd
merkys <mrkmerkys@gmail.com>
2025-02-24 17:17:27 +0200

    implemented movielist fetching

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { View, ScrollView, Text} from 'react-native';
+import MovieList from '..//services/MovieList';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Ediditng for first push.</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1}}>
+      <View style={{paddingTop: 40}}>
+        <MovieList category="popular" title="Popular movies"/>
+        <MovieList category="now_playing" title="Now playing"/>
+        <MovieList category="top_rated" title="Top rated"/>
+      </View>
+    </ScrollView>
   );
 }
---
+++ services/MovieList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Text, FlatList, ActivityIndicator, Image, StyleSheet} from 'react-native';
+import {useFetchMovies} from '../api/useFetchMovies';
+//import styles from '../styles/globalStyles';
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+interface MovieListProps {
+  category: string;
+  title: string;
+}
+
+const MovieList: React.FC<MovieListProps> = ({category, title}) => {
+  const {movies, loading, error} = useFetchMovies(category);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  if (error) {
+    return <Text>Error loading movies</Text>;
+  }
+
+  return(
+    <View>
+      <Text>{title}</Text>
+      <FlatList
+      data={movies} 
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({item}) => (
+        <View >
+          <Text>{item.title}</Text>
+        </View>
+        
+      )}
+      horizontal={true}
+      />
+    </View>
+  );
+};
+
+
+export default MovieList;
